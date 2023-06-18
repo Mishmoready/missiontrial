5a45c91bb5805358974fa93db669c5a44f4776dc
Mishmoready <moses@missionreadyhq.com>
2023-06-18 14:15:37 +1200

    Created more updates and addition as more changes be needed v0.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ProfileID from "./component/ProfileId.js";
+import BottomSection from "./component/BottomSection.js";
+import Navbar from "./component/common/Navbar";
+import Footer from "./component/common/Footer";
+import ProfilePhoto from "./component/ProfilePhoto.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <ProfileID></ProfileID>
+      <ProfilePhoto></ProfilePhoto>
+      <BottomSection></BottomSection>
+      <Footer></Footer>
     </div>
   );
 }
