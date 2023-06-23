e6d927510f42b23b27328d9de3f5300b6243e59f
Mishmoready <moses@missionreadyhq.com>
2023-06-23 16:33:52 +1200

    Revert "Created more updates and addition as more changes be needed v0.1"
    This reverts commit 5a45c91bb5805358974fa93db669c5a44f4776dc.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import "./App.css";
-import ProfileID from "./component/ProfileId.js";
-import BottomSection from "./component/BottomSection.js";
-import Navbar from "./component/common/Navbar";
-import Footer from "./component/common/Footer";
-import ProfilePhoto from "./component/ProfilePhoto.js";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <ProfileID></ProfileID>
-      <ProfilePhoto></ProfilePhoto>
-      <BottomSection></BottomSection>
-      <Footer></Footer>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
